d1b7900e86e1df5bd764d3e62b3ca9cbcacc2f86
Shireen <shireen.peters@sky.uk>
2024-05-03 19:10:04 +0100

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,15 @@
-import type { Metadata } from "next";
+import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 // import Navbar from "@/components/navbar";
-import React from "react";
 import Page from "./page"; // Import the page component
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "SpotiSky",
-  description: "Iceberg grad project",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout: React.FC = ({ children }) => {
   return (
     <html lang="en">
+      <head>{/* Add your head elements here */}</head>
       <body className={inter.className}>
         {/* <Navbar /> */}
         <Page /> {/* Render the page component */}
@@ -26,4 +17,6 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
