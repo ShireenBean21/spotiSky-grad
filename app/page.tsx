1ddf05b5b0da976b7288a728a57e8b22ae1aafce
Shireen <shireen.peters@sky.uk>
2024-05-03 17:49:25 +0100

    Fixed API issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-// pages/homepage.tsx
-
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Homepage = () => {
@@ -10,8 +8,15 @@ const Homepage = () => {
   useEffect(() => {
     const fetchFeaturedPlaylists = async () => {
       try {
-        const response = await axios.get("/api/spotify/featured-playlists");
-        setFeaturedPlaylists(response.data.playlists);
+        const response = await axios.get(
+          "https://api.spotify.com/v1/browse/featured-playlists",
+          {
+            headers: {
+              Authorization: `Bearer ${"BQAXirKga6d0yxjZwgTGnltPRlnRY-Nf9-340er8v8HOCSv2t1xA7frNSJbBuqfvL3Dm0mvvSDPUSuCA5fYM7yH6T7SOB4YqAVNLOT3-kaPa2ZuJx6g"}`,
+            },
+          }
+        );
+        setFeaturedPlaylists(response.data.playlists.items);
       } catch (error) {
         console.error("Error fetching featured playlists:", error);
       }
