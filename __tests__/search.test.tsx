b0c62d7468003606c87d69bd862af2a1cb992163
Shireen <shireen.peters@sky.uk>
2024-06-07 17:00:16 +0100

    search page test

--- __tests__/search.test.tsx
+++ __tests__/search.test.tsx
@@ -1,26 +1,61 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import SearchPage from "@/app/search/page";
+import { render, screen, waitFor } from "@testing-library/react";
+import SearchPage from "@/app/search/page"; // Adjust the import path if needed
 import React from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
+import axios from "axios";
 
+// Mock the next/navigation module
 jest.mock("next/navigation", () => ({
-  useRouter: jest.fn(),
   useSearchParams: jest.fn(),
+  useRouter: jest.fn(),
 }));
 
+// Mock axios
+jest.mock("axios");
+
 describe("SearchPage", () => {
-  //@ts-ignore
-  useRouter.mockReturnValue({ query: {} });
+  beforeEach(() => {
+    //@ts-ignore
+    // Mock useSearchParams to return a specific query
+    useSearchParams.mockReturnValue(new URLSearchParams("query=test"));
 
-  //@ts-ignore
-  useSearchParams.mockReturnValue(new URLSearchParams(""));
+    // Mock useRouter to return an empty object
+    //@ts-ignore
+    useRouter.mockReturnValue({});
 
-  it("renders without crashing", () => {
+    // Mock axios.get to return a mock response
+    //@ts-ignore
+    axios.get.mockResolvedValue({
+      data: {
+        tracks: {
+          items: [
+            {
+              id: "1",
+              name: "Track 1",
+              type: "track",
+              images: [{ url: "/track1.jpg" }],
+            },
+          ],
+        },
+        artists: { items: [] },
+        albums: { items: [] },
+        playlists: { items: [] },
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("renders search results based on the query", async () => {
     render(<SearchPage />);
 
-    const searchResults = screen.getByText("searchResults");
+    // Use waitFor to wait for the asynchronous rendering of search results
+    //@ts-ignore
+    const searchResult = await waitFor(() => screen.getByText("Track 1"));
 
-    expect(searchResults).toBeInTheDocument();
+    expect(searchResult).toBeInTheDocument();
   });
 });
